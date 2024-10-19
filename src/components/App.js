67419851c67cfe90deeee2afb38e7e5b047fdbc3
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-19 20:42:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+const male=["Shirts", "Facial-grooming kits", "Hair styling", "Jeans", "Shoes"];
+const female=["Tops", "Skirts", "Makeup items", "Jewellery", "Sandals"];
 const App = () => {
+  const[gender, setGender]=useState("");
+  
+  function changeGender(e){
+       setGender(e.target.value);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        
+        <select value={gender} onChange={changeGender}>
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+        </select>
+        <ul>
+          {
+            gender==="male" ? male.map(item=><li>{item}</li>) : female.map(item=><li>{item}</li>)
+          }
+        </ul>
     </div>
   )
 }
